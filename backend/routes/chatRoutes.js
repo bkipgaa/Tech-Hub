80cc6abfcfbeb4d18667e826f0906fbc269f91f9
bkipgaa <bkipgaa@gmail.com>
2026-08-16 01:36:51 +0300

    message system

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const chatController = require('../controllers/chatController');
 
 /**
