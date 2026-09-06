420924717d71fc0e248d53e99c98eb2283d82be6
bkipgaa <bkipgaa@gmail.com>
2026-09-07 02:15:38 +0300

    Updated work flow

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middleware/auth'); // your auth middleware
-const bookingController = require('../controllers/bookingController');
+const bookingController = require('../controllers/bookingratingController');
 
 // ──────────────────────────────────────────────────────────────
 // All booking routes require authentication
